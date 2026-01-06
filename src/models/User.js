64c66f9cddf4e9fb162c3583753f25c5e643ea70
sajalnew01 <sajalnew01@github.com>
2026-01-06 23:20:47 +0530

    Fix mongoose pre-save hook (remove next)

--- src/models/User.js
+++ src/models/User.js
@@ -11,10 +11,9 @@ const userSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-userSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) return next();
+userSchema.pre("save", async function () {
+  if (!this.isModified("password")) return;
   this.password = await bcryptjs.hash(this.password, 10);
-  next();
 });
 
 module.exports = mongoose.model("User", userSchema);
